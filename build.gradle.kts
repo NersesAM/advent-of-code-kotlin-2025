bfb0405ecad7a58cc3403bff2a39170a761925bc
Nerses Aznauryan <1935500+NersesAM@users.noreply.github.com>
2025-12-05 13:26:35 +0000

    utils

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,19 @@ tasks {
         gradleVersion = "9.2.1"
     }
 }
+
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_24
+  targetCompatibility = JavaVersion.VERSION_24
+}
+
+//kotlin {
+//  compilerOptions {
+//    freeCompilerArgs.add("-Xnon-local-break-continue")
+//  }
+//}
+
+//dependencies {
+//  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+//}
